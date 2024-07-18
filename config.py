1c240d4ecbccb727f64d58360dd9a4a700760512
Aakashraj24 <129396513+Aakashraj24@users.noreply.github.com>
2024-07-18 09:03:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ API_ID = os.environ.get("API_ID", "29201763")
 
 API_HASH = os.environ.get("API_HASH", "10d0e463862b3d0e4c2441aa70b73c8f")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7241587663:AAHrvXelS3YBr9_6i0srJjIJuAWQz0--Aso") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "-1002155925318") 
 
 DB_NAME = os.environ.get("DB_NAME","kohbots")     
 
